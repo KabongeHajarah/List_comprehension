f74159b66b837e4c26d7a06ce126714dda56bede
KabongeHajarah <kabongehajarah@gmail.com>
2023-06-02 00:42:59 +0300

    assignment

--- list_chpsn.py
+++ list_chpsn.py
@@ -68,14 +68,18 @@ print(two_power_of_five)
 
              #EXERCISES
 # Filter only negative and zero in the list using list comprehension
+def filter_numbers():
+ numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+ only_negative_and_zero=[number for number in numbers if number<=0]
+ return only_negative_and_zero
+print(filter_numbers())
 
-# numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
 # Flatten the following list of lists of lists to a one dimensional list :
-
-# list_of_lists =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
-
-# output
-# [1, 2, 3, 4, 5, 6, 7, 8, 9]
+def flatten():
+ list_of_lists =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+ flattened_list=[number  for sublist in  list_of_lists for row in sublist for number in row]
+ return flattened_list
+print(flatten())
 # Using list comprehension create the following list of tuples:
 
 # [(0, 1, 0, 0, 0, 0, 0),
@@ -89,21 +93,41 @@ print(two_power_of_five)
 # (8, 1, 8, 64, 512, 4096, 32768),
 # (9, 1, 9, 81, 729, 6561, 59049),
 # (10, 1, 10, 100, 1000, 10000, 100000)]
+result = [(i, 1, i, i**2, i**3, i**4, i**5) for i in range(11)]
+print(result)
+
+
 # Flatten the following list to a new list:
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+flatten = [[country.upper(), country[:3].upper(), city.upper()] for sublist in countries for (country, city) in sublist]
+print(flatten)
 
-# countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
-# output:
+ # output:
 # [['FINLAND','FIN', 'HELSINKI'], ['SWEDEN', 'SWE', 'STOCKHOLM'], ['NORWAY', 'NOR', 'OSLO']]
-# Change the following list to a list of dictionaries:
 
-# countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+
+# Change the following list to a list of dictionaries:
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
 # output:
 # [{'country': 'FINLAND', 'city': 'HELSINKI'},
 # {'country': 'SWEDEN', 'city': 'STOCKHOLM'},
 # {'country': 'NORWAY', 'city': 'OSLO'}]
-# Change the following list of lists to a list of concatenated strings:
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+dictionaries = [{'country': country.upper(), 'city': city.upper()} for sublist in countries for (country, city) in sublist]
+print(dictionaries)
 
-# names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
+
+
+# Change the following list of lists to a list of concatenated strings:
+names = [[('Asabeneh', 'Yetayeh')], [('David', 'Smith')], [('Donald', 'Trump')], [('Bill', 'Gates')]]
 # output
 # ['Asabeneh Yetaeyeh', 'David Smith', 'Donald Trump', 'Bill Gates']
+
+concat=[(i +' '+ n) for sublist in names for (i,n) in sublist ]
+print(concat)
+
+
 # Write a lambda function which can solve a slope or y-intercept of linear functions.
+slope=lambda m,x,c: (m*x)+c
+print(slope(1,2,3))
